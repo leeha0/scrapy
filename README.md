a9f878522893e38cf04fd279ed1ba0931212f522
leeha0 <gkdud2837@gmail.com>
2018-07-17 10:26:47 +0900

    Detail Page 수집 및 README 정리

--- README.md
+++ README.md
@@ -1,21 +1,38 @@
-# Scrapy
+# Scrapy-Test
 ```
 Scrapinghub 테스트를 위한 프로젝트
 ```
 
-## Install
+* [Scrapinghub](https://scrapinghub.com/) - Turn websites into data
 
-### install Scrapy 
+## Getting Started
+
+### Prerequisites 
+Python 2.7 or Python3.4
+
+### Installing
+* Install Scrapy Lib
 ```
 pip install Scrapy
+```
+* Create Tutorial Project
+```
 scrapy startproject tutorial
 ```
 
-### Deploy
+## Deploy
+### Installing
+* Install shub Lib
 ```
 pip install shub
+```
+* Setting 
+```
 shub login
 > Enter Your Key
+```
+* Deploy
+```
 shub deploy 
 > Enter Your Project ID
 ```
@@ -33,5 +50,3 @@ scrapy shell <fetchUrl>
 scrapy runspider quotes.py -o items.json
 scrapy crawl quote
 ```
-
-* [Scrapinghub](https://scrapinghub.com/) - Turn websites into data
--- tutorial/tutorial/spiders/QuotesSpider.py
+++ tutorial/tutorial/spiders/QuotesSpider.py
@@ -20,15 +20,30 @@ class QuotesSpider(scrapy.Spider):
         self.log('visited: ' + response.url)
 
         for quote in response.css('div.quote'):
-            yield {
+            item = {
                 'text': quote.css('span.text::text').extract_first(),
                 'author': quote.css('span small::text').extract_first(),
                 'tags': quote.css('div.tags a.tag::text').extract(),
             }
+            yield item
 
         next_page = response.css('li.next a::attr(href)').extract_first()
-        if next_page is not None:
+        # next_page = response.urljoin(next_page)
+        # yield scrapy.Request(url=next_page, callback=self.parse)
+        if next_page:
             yield response.follow(next_page, callback=self.parse)
+
+        # details view
+        # urls = response.css('div.quote > span > a::attr(href)').extract()
+        # for url in urls:
+        #     url = response.urljoin(url)
+        #     yield scrapy.Request(url=url, callback=self.parse_details)
+
+    def parse_details(self, response):
+        yield {
+            'name': response.css('h3.author-title::text').extract_first(),
+            'birth_datte': response.css('span.author-born-date::text').extract_first(),
+        }
 
         # file write
         # page = response.url.split('/')[-2]
